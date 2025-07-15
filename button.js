60bb1d0a660a5ad40f9331485b27d5b9014fed31
luluchoue <lucressemata237@gmail.com>
2025-07-15 12:36:56 +0200

    java

---
+++ button.js
@@ -0,0 +1,11 @@
+
+
+
+let btn =document.querySelector("button");
+console.log(btn);
+
+btn.addEventListener('click',
+    ()=>{
+        alert('click du bouton');
+    }
+);
---
+++ script.js
@@ -0,0 +1,64 @@
+
+
+let nbre =3;
+nbre =14;
+console.log(nbre);
+const nbre2 =5;
+
+let firstName ="waffo";
+console.log(firstName);
+
+
+
+let fullName ="waffo lele Rostand";
+console.log(fullName);
+
+console.log(typeof(fullName));
+console.log(typeof(nbre2));
+
+
+let fruits = ['orangre','pomme', 'banane', 'fraise','kiwi', 'ananas','mangue'];
+
+let fruit2 = ['orangre','apple', 'banana', 'strawbery','kiwi', 'pineapple','mango'];
+
+console.log(fruits);
+console.log(fruit2);
+console.log(typeof(fruits));
+
+//document.write(fruit2);
+//alert('hello word')
+
+//prompt('entrer votre nom');
+
+//let nom ="";
+//nom =prompt('entrer votre nom');
+//document.write("bonjour prof: "+nom);
+
+//let a=prompt('entrer le nombre 1');
+//let b=prompt('entrer le nombre 2');
+
+//let prod =0;
+//document.write("le produit est: " +prod);
+
+//a=parseInt(a);
+//b=parseInt(b);
+
+//sous = a-b;
+//som = a+b
+
+//let som =0;
+//let sous =0;
+//a=parseInt(a);
+//b=parseInt(b);
+//sous = a-b;
+//som = a+b;
+   //if(a>b){
+   // document.write("la soustraction est : "  +sous);
+   // } 
+
+    //if(a<b){ 
+//document.write("la somme est: "  +som); }
+
+
+
+//var nbre3 = 17;
